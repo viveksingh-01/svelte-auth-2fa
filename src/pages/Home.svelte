684735be1aa1c50574b576e951b8938e4122704b
viveksingh-01 <viveksingh9301@gmail.com>
2022-05-07 15:58:01 +0530

    feat: fetch and show user's info on home-page

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,36 @@
+<script lang="ts">
+  import axios from 'axios';
+  import { onMount } from 'svelte';
+
+  interface IUser {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+  }
+  let user: IUser;
+
+  async function getUserInfo() {
+    try {
+      const { data } = (await axios.get('http://localhost:8000/api/user')) as {
+        data: IUser;
+      };
+      user = data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  onMount(() => {
+    getUserInfo();
+  });
+</script>
+
 <section>
   <div class="has-text-centered my-5">
-    <h2 class="is-size-2">Welcome to Svelte 2FA Auth!</h2>
+    {#if user?.firstName}
+      <h2 class="is-size-2">Hi {user.firstName}!</h2>
+    {/if}
+    <h3 class="is-size-3">Welcome to Svelte 2FA Auth!</h3>
   </div>
 </section>
