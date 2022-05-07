515dfe2f4463b325819726bed453150df678e430
viveksingh-01 <viveksingh9301@gmail.com>
2022-05-08 00:56:06 +0530

    refactor: update response interceptor's logic

--- src/interceptors/axios.ts
+++ src/interceptors/axios.ts
@@ -5,16 +5,18 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:8000/api';
 
-let isRefreshDone = false;
+let isRefreshAttempted = false;
 
 axios.interceptors.response.use(res => res, async error => {
-  if (!isRefreshDone && error.response.status === 401) {
-    const res: {data: {token: string}, status: number} = await axios.post('refresh', {}, {withCredentials: true}) ;
-    isRefreshDone = true;
-    if (res.status === 200) {
-      axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.token}`;
-      return axios(error.config);    
-    }
+  if (!isRefreshAttempted) {
+    isRefreshAttempted = true;
+    if (error.response.status === 401) {
+      const res: {data: {token: string}, status: number} = await axios.post('refresh', {}, {withCredentials: true}) ;
+      if (res.status === 200) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.token}`;
+      }
+    } 
+    return axios(error.config);    
   }
   return error;
 })
