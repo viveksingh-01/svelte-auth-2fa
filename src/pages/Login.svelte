3eb3ff8ebeafcf9c592ada302904f5497302ae85
viveksingh-01 <viveksingh9301@gmail.com>
2022-05-07 12:36:52 +0530

    feat: implement user login feature

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,30 +1,64 @@
+<script lang="ts">
+  import axios from 'axios';
+  let email = '',
+    password = '';
+
+  interface ILoginPayload {
+    email: string;
+    password: string;
+  }
+
+  async function login(payload: ILoginPayload) {
+    try {
+      const { data } = await axios.post(
+        'http://localhost:8000/api/login',
+        payload
+      );
+      console.log(data?.message);
+    } catch (error) {
+      console.error(error?.response?.data.message);
+    }
+  }
+
+  function submitForm() {
+    const payload: ILoginPayload = { email, password };
+    login(payload);
+  }
+</script>
+
 <section class="form__container">
   <h2 class="mb-4 is-size-2">Login</h2>
-  <div class="content">
-    <div class="field">
-      <label class="label" for="email">Email</label>
-      <div class="control">
-        <input
-          type="text"
-          placeholder="Enter your email address"
-          class="input"
-        />
+  <form on:submit|preventDefault={submitForm}>
+    <div class="content">
+      <div class="field">
+        <label class="label" for="email">Email</label>
+        <div class="control">
+          <input
+            type="email"
+            placeholder="Enter your email address"
+            class="input"
+            bind:value={email}
+          />
+        </div>
+      </div>
+      <div class="field">
+        <label class="label" for="password">Password</label>
+        <div class="control">
+          <input
+            type="password"
+            placeholder="Enter your password"
+            class="input"
+            bind:value={password}
+          />
+        </div>
+      </div>
+      <div class="field mt-5">
+        <div class="control">
+          <button type="submit" class="button is-info is-fullwidth">
+            Login
+          </button>
+        </div>
       </div>
     </div>
-    <div class="field">
-      <label class="label" for="password">Password</label>
-      <div class="control">
-        <input
-          type="password"
-          placeholder="Enter your password"
-          class="input"
-        />
-      </div>
-    </div>
-    <div class="field mt-5">
-      <div class="control">
-        <button class="button is-info is-fullwidth"> Login </button>
-      </div>
-    </div>
-  </div>
+  </form>
 </section>
