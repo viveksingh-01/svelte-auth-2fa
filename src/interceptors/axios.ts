cf8c08d4a05752f9cf09270dbebecd7edb2ab13b
viveksingh-01 <viveksingh9301@gmail.com>
2022-05-08 15:57:34 +0530

    fix: fix firing of subsequent request in case of error except 401

--- src/interceptors/axios.ts
+++ src/interceptors/axios.ts
@@ -10,19 +10,19 @@ axios.defaults.baseURL = 'http://localhost:8000/api';
 let isRefreshAttempted = false;
 
 axios.interceptors.response.use(res => res, async error => {
-  if (!isRefreshAttempted) {
-    isRefreshAttempted = true;
-    if (error.response.status === 401) {
+  if (error.response.status === 401) {
+    if (!isRefreshAttempted) {
+      isRefreshAttempted = true;
       const res: {data: {token: string}, status: number} = await axios.post('refresh', {}, {withCredentials: true}) ;
       if (res.status === 200) {
         axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.token}`;
+        return axios(error.config);    
       }
     } 
-    return axios(error.config);    
   }
   setMessage({
     body: error.response.data.message,
     type: 'error'
-  })
+  });
   return error;
-})
+});
